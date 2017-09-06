e75d97379ac97e6c3973a7547338bd1bd7d2207e
janikibichi <janikibichi@gmail.com>
2017-09-06 17:34:35 +0300

    levels

---
+++ PlatformGame/app/src/main/java/com/mjuaji/platformgame/PlatformView.java
@@ -0,0 +1,66 @@
+package com.mjuaji.platformgame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class PlatformView extends SurfaceView implements Runnable {
+    private boolean debugging = true;
+    private volatile boolean running;
+    private Thread gameThread = null;
+    //For drawing
+    private Paint paint;
+    private Canvas canvas;
+    private SurfaceHolder ourHolder;
+    Context context;
+    long startFrameTime;
+    long timeThisFrame;
+    long fps;
+    //new engine classes
+    private LevelManager lm;
+    private Viewport vp;
+    InputController ic;
+
+    public PlatformView(Context context) {
+        super(context);
+        this.context = context;
+
+        //initialize our drawing objects
+        ourHolder = getHolder();
+        paint = new Paint();
+    }
+    @Override
+    public void run() {
+        while(running){
+            startFrameTime = System.currentTimeMillis();
+            update();
+            draw();
+
+            //Calculate the fps this frame, use result to time animations/movement.
+            timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            if(timeThisFrame >= 1){
+                fps = 1000 / timeThisFrame;
+            }
+        }
+    }
+    private void update(){
+        //new update() code will go here
+    }
+
+    private void draw(){
+        if(ourHolder.getSurface().isValid()){
+            //first we lock the area of memory we will be drawing to
+            canvas = ourHolder.lockCanvas();
+
+            //rub out last frame with arbitrary color
+            paint.setColor(Color.argb(255,0,0,255);
+            canvas.drawColor(Color.argb(255,0,0,255));
+
+            //unlock and draw the scene
+            ourHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+}
