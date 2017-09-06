7c7a91a598adf7af2f63fb26ffe864e07d727513
janikibichi <janikibichi@gmail.com>
2017-09-06 11:46:04 +0300

    clean up

--- TappyDefenderV1/app/src/main/java/com/mjuaji/tappydefenderv1/MainActivity.java
+++ TappyDefenderV1/app/src/main/java/com/mjuaji/tappydefenderv1/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -42,5 +43,14 @@ public class MainActivity extends Activity implements View.OnClickListener{
      startActivity(i);
      //shut this activity down
      finish();
+    }
+
+    //handle the back button
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            finish();
+            return true;
+        }
+        return false;
     }
 }
