76fe8f56462bc8d72d2889562c98c30d9078984c
janikibichi <janikibichi@gmail.com>
2017-09-10 10:55:38 +0300

    new objects

---
+++ PlatformGame/app/src/main/java/com/mjuaji/platformgame/Stalactite.java
@@ -0,0 +1,27 @@
+package com.mjuaji.platformgame;
+
+import java.util.Random;
+
+public class Stalactite extends GameObject {
+    Stalactite(float worldStartX, float worldStartY, char type){
+        final float HEIGHT = 3;
+        final float WIDTH = 2;
+        setHeight(HEIGHT);
+        setWidth(WIDTH);
+        setType(type);
+        setBitmapName("stalactite");
+        setActive(false);
+        //Randomly set the tree either just in front or just behind the player -1 or 1
+        Random rand = new Random();
+        if(rand.nextInt(2)==0){
+            setWorldLocation(worldStartX, worldStartY, -1);
+        }else{
+            setWorldLocation(worldStartX, worldStartY, 1);
+        }
+        //no stalagmite
+    }
+    @Override
+    public void update(long fps, float gravity) {
+
+    }
+}
